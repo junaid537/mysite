ca158632610ea27c776fe9f27ad9f606a6910c73
Mohammed junaid Iqbal khalidi <moiqbal@Mohammeds-MacBook-Pro.local>
2025-06-03 12:50:27 -0700

    frst comit

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -7,6 +7,7 @@ import { loadFragment } from '../fragment/fragment.js';
  */
 export default async function decorate(block) {
   // load footer as fragment
+  console.log(block);
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
@@ -15,6 +16,5 @@ export default async function decorate(block) {
   block.textContent = '';
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
   block.append(footer);
 }
